5c01726da74e6ebafc0ab69748c4dba6e976b7a8
Akash Sahu <akkusahu559@gmail.com>
2020-12-16 22:58:53 +0530

    Solution Added

---
+++ Basic/Array/03. Balanced Array.cpp
@@ -0,0 +1,18 @@
+
+// Que 03). Balanced Array. 
+// Que Link :- https://bit.ly/37ngCeT
+
+// Implementation
+
+// 1st Approach in CPP
+// Time Complexity = O(N), Space Complexity = O(1)
+
+int minValueToBalance(int a[], int n) {
+   int minValue, sum1 = 0, sum2 = 0;
+   for(int itr = 0, btr = n - 1; itr < n / 2; ){
+       sum1 += a[itr++];
+       sum2 += a[btr--];
+   }
+   if(sum1 >= sum2) return minValue = sum1 - sum2;
+   else return minValue = sum2 - sum1;
+}
