1aadd51600810c2845974228ca26c852e0cb66d8
Akash Sahu <akkusahu559@gmail.com>
2020-12-16 23:01:06 +0530

    Another approach added in Java

--- Basic/Array/03. Balanced Array.cpp
+++ Basic/Array/03. Balanced Array.cpp
@@ -16,3 +16,17 @@ int minValueToBalance(int a[], int n) {
    if(sum1 >= sum2) return minValue = sum1 - sum2;
    else return minValue = sum2 - sum1;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(1)
+
+long minValueToBalance(long a[] ,int n) {
+        int minValue, sum1 = 0, sum2 = 0;
+        for(int itr = 0, btr = n - 1; itr < n / 2; ){
+            sum1 += a[itr++];
+            sum2 += a[btr--];
+        }
+        if(sum1 >= sum2) return minValue = sum1 - sum2;
+        else return minValue = sum2 - sum1;
+    }
