de43f0b43b343592cfd2114054f7e69c585ec29c
Akash Sahu <akkusahu559@gmail.com>
2020-12-17 20:44:39 +0530

    Solution Added

---
+++ Basic/Array/04. Find triplets with zero sum.cpp
@@ -0,0 +1,23 @@
+
+// Que 04). Find triplets with zero sum.
+// Que Link :- https://practice.geeksforgeeks.org/problems/find-triplets-with-zero-sum/1
+
+// Implementation
+
+// 1st Approach in CPP
+// Time Complexity = O(N^2), Space Complexity = O(1)
+
+bool findTriplets(int arr[], int n)
+{ 
+    bool ans = false;
+    sort(arr, arr + n);
+    for(int loop = 0; loop < n-2; loop++){
+        int itr = loop + 1, jtr = n - 1;
+        while(itr < jtr){
+            if((arr[loop] + arr[itr] + arr[jtr]) == 0) return true;
+            else if((arr[loop] + arr[itr] + arr[jtr]) < 0) itr++;
+            else jtr--;
+        }
+    }
+    return false;
+}
