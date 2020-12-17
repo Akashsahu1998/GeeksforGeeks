a40d08cd506ed5c8a805b07a10568e032c4bcc26
Akash Sahu <akkusahu559@gmail.com>
2020-12-17 20:50:10 +0530

    Another solution added in Java

--- Basic/Array/04. Find triplets with zero sum.cpp
+++ Basic/Array/04. Find triplets with zero sum.cpp
@@ -21,3 +21,22 @@ bool findTriplets(int arr[], int n)
     }
     return false;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N^2), Space Complexity = O(1)
+
+public boolean findTriplets(int arr[] , int n)
+{
+    boolean ans = false;
+    Arrays.sort(arr);
+    for(int loop = 0; loop < n-2; loop++){
+        int itr = loop + 1, jtr = n - 1;
+        while(itr < jtr){
+            if((arr[loop] + arr[itr] + arr[jtr]) == 0) return true;
+            else if((arr[loop] + arr[itr] + arr[jtr]) < 0) itr++;
+            else jtr--;
+        }
+    }
+    return false;
+}
