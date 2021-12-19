6efb229f849cea0a8788ae54e1541d2c82a98f04
Akash Sahu <akkusahu559@gmail.com>
2021-12-19 18:21:41 +0530

    Using BFS

--- Easy/Tree/01. Left View of Binary Tree.cpp
+++ Easy/Tree/01. Left View of Binary Tree.cpp
@@ -18,3 +18,28 @@ vector<int> leftView(Node *root){
     findRightSideView(root, res, 1);
     return res;
 }
+
+
+// Using BFS
+// Time Complexity = O(N), Space Complexity = O(N)
+vector<int> leftView(Node *root){
+   vector<int> res;
+    if(!root) return res;
+    
+    queue<Node*> q;
+    q.push(root);
+    
+    while(!q.empty()){
+        int size = q.size();
+        res.push_back(q.front()->data);
+        
+        for(int itr = 0; itr < size; itr++){
+            Node* temp = q.front();
+            q.pop();
+            
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right);
+        }
+    }
+    return res;
+}
