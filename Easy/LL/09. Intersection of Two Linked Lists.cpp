b0acef6b7585d0e6cf5a35b1f21854300921ffa7
Akash Sahu <akkusahu559@gmail.com>
2021-12-23 09:00:13 +0530

    Using unordered set, with comments

---
+++ Easy/LL/09. Intersection of Two Linked Lists.cpp
@@ -0,0 +1,43 @@
+
+// Que) 9. Intersection of Two Linked Lists 
+// Que Link :- https://practice.geeksforgeeks.org/problems/intersection-of-two-linked-list/1
+
+// Implementation
+
+// Using unrodered_set
+// Time Complexity = O(N), Space Complexity = O(N)
+class Solution
+{
+    public:    
+    Node* findIntersection(Node* head1, Node* head2){
+        unordered_set<int> st;
+        
+        // Storing the Node data in the set
+        Node* cur = head2;        
+        while(cur){
+            st.insert(cur->data);
+            cur = cur->next;
+        }
+        
+        Node* res = new Node(0);
+        Node* resCur = res;
+        
+        // Iteratign & Checking if node is already present into map,
+        // it means that is the intersection point.
+        cur = head1;
+        while(cur){
+            if(st.find(cur->data) != st.end()) {
+                Node* dummy = new Node(cur->data);
+                resCur->next = dummy;
+                resCur = resCur->next;
+            }
+            cur = cur->next;
+        }
+        
+        resCur->next = NULL;
+        
+        // returnign res->next, bcz res points to 0
+        return res->next;
+    }
+};
+
