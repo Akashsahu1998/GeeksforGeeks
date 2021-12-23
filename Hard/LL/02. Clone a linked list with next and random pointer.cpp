10afe5d59893a0afe2a3faed735a0db1b37a12fc
Akash Sahu <akkusahu559@gmail.com>
2021-12-23 07:43:28 +0530

    Efficient Approach

--- Hard/LL/02. Clone a linked list with next and random pointer.cpp
+++ Hard/LL/02. Clone a linked list with next and random pointer.cpp
@@ -48,3 +48,52 @@ class Solution
         return copy->next;
     }
 };
+
+
+// Efficient Approach
+// Time Complexity = O(N), Space Complexity = O(1)
+class Solution
+{
+    public:
+    Node *copyList(Node *head)
+    {
+        //Write your code here
+        if(!head) return NULL;
+        
+        // Building the dummyNode after each and every original node(cur)
+        // pointing the cur->next to dummyNode & dummyNode to cur->next->next
+        Node *cur = head;        
+        while(cur){
+            Node *front = cur->next;
+            Node *dummyNode = new Node(cur->data);
+            cur->next = dummyNode;
+            dummyNode->next = front;
+            cur = front;
+        }
+        
+        // Pointing the randomNode in each and every dummy node we created
+        cur = head;
+        while(cur){
+            if(cur->arb){
+                // we are putting random node(cur->random->next) in dummyNode(cur->next->random) only
+                cur->next->arb = cur->arb->next;
+            }
+            cur = cur->next->next;
+        }
+        
+        // Pointing the copy to the first dummy node, so we can return it.
+        cur = head;
+        Node* copy = new Node(0);
+        Node* copyCur = copy;
+        
+        // deleting the links of org nodes to dummy nodes
+        while(cur){            
+            copyCur->next = cur->next;
+            cur->next = cur->next->next;
+            cur = cur->next;
+            copyCur = copyCur->next;
+        }
+		// return copy->next, bcz copy was pointing to 0
+        return copy->next;
+    }
+};
