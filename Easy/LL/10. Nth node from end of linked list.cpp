eafec8ecf17b55fae4f7105cf1b229f431d1db07
Akash Sahu <akkusahu559@gmail.com>
2021-12-24 05:51:37 +0530

    Using two pointer, with comments

--- Easy/LL/10. Nth node from end of linked list.cpp
+++ Easy/LL/10. Nth node from end of linked list.cpp
@@ -7,19 +7,22 @@
 // using two pointer
 // Single Traversal
 // Time Complexity = O(N), Space Complexity = O(1)
-int getNthFromLast(struct Node *head, int n){
-    struct Node *ptr1 = head, *ptr2 = head;
+int getNthFromLast(Node *head, int n){
+       Node *ptr1 = head, *ptr2 = head;
     
-    while(ptr1){
-        if(n > 0){
-            ptr1 = ptr1->next;
-            n--;
+        while(ptr1){
+            // movign the ptr1 n steps ahead of ptr2,
+            // so when ptr1 reaches null it means ptr2 will be
+            // standing on a nth node from last
+            if(n > 0){
+                ptr1 = ptr1->next;
+                n--;
+            }
+            else{
+                ptr1 = ptr1->next;
+                ptr2 = ptr2->next;
+            }
         }
-        else{
-            ptr1 = ptr1->next;
-            ptr2 = ptr2->next;
-        }
-    }
-    return (n) ? -1 : ptr2->data;
+        return (n) ? -1 : ptr2->data;
 }
 
