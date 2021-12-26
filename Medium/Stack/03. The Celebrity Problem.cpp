49e91a9921ca48cdbce8d47c3647df5146c451fc
Akash Sahu <akkusahu559@gmail.com>
2021-12-26 13:57:05 +0530

    Using stack efficient approach

--- Medium/Stack/03. The Celebrity Problem.cpp
+++ Medium/Stack/03. The Celebrity Problem.cpp
@@ -30,3 +30,44 @@ class Solution
         return -1;
     }
 };
+
+
+// reference : https://www.youtube.com/watch?v=CiiXBvrX-5A
+// Efficient Approach
+// Using Stack
+// Time Complexity = O(N), Space Complexity = O(N)
+class Solution 
+{
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& matrix, int n) {
+        stack<int> st;
+        for(int itr = 0; itr < n; itr++){
+            st.push(itr);
+        }
+        
+        while(st.size() > 1){
+            int itr = st.top();
+            st.pop();
+            int jtr = st.top();
+            st.pop();
+            if(matrix[itr][jtr]){
+                // itr knows jtr
+                st.push(jtr);
+            }
+            else{
+                // jtr knows itr
+                st.push(itr);
+            }
+        }
+        
+        int lastValue = st.top();
+        st.pop();
+        for(int itr = 0; itr < n; itr++){
+            if(itr != lastValue && (matrix[itr][lastValue] == 0 || matrix[lastValue][itr] == 1)){
+                 return -1;
+            }
+        }
+        return lastValue;
+    }
+};
