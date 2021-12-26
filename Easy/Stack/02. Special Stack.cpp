064048195e997afaa6d4915561556872ee0e9d06
Akash Sahu <akkusahu559@gmail.com>
2021-12-26 12:24:14 +0530

    O(1) time but O(1) space

--- Easy/Stack/02. Special Stack.cpp
+++ Easy/Stack/02. Special Stack.cpp
@@ -4,6 +4,7 @@
 
 // Implementation
 
+// Not an efficient approach
 // Time Complexity = O(1), Space Complexity = O(N)
 void push(stack<int>& s, int a){
 	int minValue;
@@ -38,4 +39,47 @@ int pop(stack<int>& s){
 
 int getMin(stack<int>& s){
 	return s.top();
+}
+
+
+// Efficient Approach
+// Time Complexity = O(1), Space Complexity = O(1)
+int minVal;
+void push(stack<int>& s, int a){
+    if(!s.size()){
+        s.push(a);
+        minVal = a;
+    }
+    if(a < minVal){
+        s.push(a*2 - minVal);
+        minVal = a;
+    }
+    else s.push(a);
+}
+
+bool isFull(stack<int>& s,int n){
+	if(s.size() == n) return true;
+	return false;
+}
+
+bool isEmpty(stack<int>& s){
+	if(s.size()) return false;
+	return true;
+}
+
+int pop(stack<int>& s){
+    if(!s.size()) return -1;
+    int top = s.top();
+    int returnVal = top;
+    s.pop();
+    if(top < minVal){
+        returnVal = minVal;
+        minVal = minVal*2 - top;
+    }
+    else returnVal = top;
+    return returnVal;
+}
+
+int getMin(stack<int>& s){
+	return minVal;
 }
