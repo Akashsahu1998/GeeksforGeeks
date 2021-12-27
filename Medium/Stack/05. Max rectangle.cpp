f4d8da252f1f0bb4cf5e971be35f16bf390af0f9
Akash Sahu <akkusahu559@gmail.com>
2021-12-27 09:01:22 +0530

    Most efficient approach

---
+++ Medium/Stack/05. Max rectangle.cpp
@@ -0,0 +1,112 @@
+
+// Que) 01. Max rectangle
+// Que Link :- https://practice.geeksforgeeks.org/problems/max-rectangle/1
+
+// Implementation
+
+// Efficient Approach
+// Time Complexity = O(Row Col) + O(Col) = O(Row*Col)
+// Space Complexity = O(Col)
+class Solution{
+  public:
+  	// Traversing 3 time 
+//    int findTheLargestRectangeInHistogram(vector<int> heights){
+//        int n = heights.size();
+//        vector<int> leftArr(n), rightArr(n);
+//        stack<int> st;
+//
+//        // finding element on left which is lesser than the current element
+//        for(int itr = 0; itr < n; itr++){
+//            while(!st.empty() && heights[st.top()] >= heights[itr]){
+//                st.pop();
+//            }
+//
+//            // if stack is empty then put 0
+//            if(st.empty()){
+//                leftArr[itr] = 0;
+//            }
+//            else{
+//                leftArr[itr] = st.top() + 1;
+//            }
+//
+//            st.push(itr);
+//        }
+//
+//        // clear stack, to create rightArr
+//        while(!st.empty()){
+//            st.pop();
+//        }
+//
+//        // finding element on right which is lesser than the current element
+//        for(int itr = n-1; itr >= 0; itr--){
+//            while(!st.empty() && heights[st.top()] >= heights[itr]){
+//                st.pop();
+//            }
+//
+//            // if stack is empty then put n-1
+//            if(st.empty()){
+//                rightArr[itr] = n-1;
+//            }
+//            else{
+//                rightArr[itr] = st.top() - 1;
+//            }
+//
+//            st.push(itr);
+//        }
+//
+//        // calculating the max rectangle by using left & right less current value
+//        int ans = -1;
+//        for(int itr = 0; itr < n; itr++){
+//            int sum = (rightArr[itr] - leftArr[itr] + 1) * heights[itr];
+//            ans = max(ans, sum);
+//        }
+//
+//        return ans;
+//    }
+    
+    // Traversing 1 time 
+    int findTheLargestRectangeInHistogram(vector<int>& heights) {
+        stack<int> s;
+        int itr = 0, maxArea = -1;
+        while(itr < heights.size()){
+            if(s.empty() || heights[s.top()] <= heights[itr]){
+                s.push(itr++);
+            }
+            else{
+                int top = s.top();
+                s.pop();
+                int area = heights[top] * (s.empty() ? itr
+                                                    : itr - s.top() - 1);
+                maxArea = max(maxArea, area);
+            }            
+        }   
+        
+        while(!s.empty()){
+            int top = s.top();
+            s.pop();
+            int area = heights[top] * (s.empty() ? itr
+                                                : itr - s.top() - 1);
+            maxArea = max(maxArea, area);
+        }
+
+        return maxArea;
+    }
+  
+    int maxArea(int matrix[MAX][MAX], int n, int m) {
+        int ans = 0;
+        vector<int> rowArr(m);
+        for(int itr = 0; itr < n; itr++){            
+            for(int jtr = 0; jtr < m; jtr++){
+                if(itr == 0) rowArr[jtr] = matrix[itr][jtr];
+                else {  // if value is 1 then add it with the value of rowArr otherwise simple put 0
+                    if(matrix[itr][jtr] == 1) rowArr[jtr] += matrix[itr][jtr];
+                    else rowArr[jtr] = 0;
+                }
+            }
+            ans = max(ans, findTheLargestRectangeInHistogram(rowArr));
+        }
+        
+        // using the rectangle in histogram solution
+        return ans;
+    }
+};
