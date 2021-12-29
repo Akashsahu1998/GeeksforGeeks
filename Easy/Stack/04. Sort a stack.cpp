5f3f6320f2cb7684932cf1bd1feb45eee017bf16
Akash Sahu <akkusahu559@gmail.com>
2021-12-29 08:01:24 +0530

    Using recursion

--- Easy/Stack/04. Sort a stack.cpp
+++ Easy/Stack/04. Sort a stack.cpp
@@ -4,6 +4,7 @@
 
 // Implementation
 
+// 1st Approach
 // Using Multiset
 // Time Complexity = O(NLogN), Space Complexity = O(N)
 void SortedStack :: sort(){
@@ -16,4 +17,34 @@ void SortedStack :: sort(){
    for(auto itr : st){
        s.push(itr);
    }
+}
+
+
+// 2nd Approach
+// Using Recursion
+// for every n element we are traversing n element
+// i.e => [3, 2, 1]
+// Time Complexity = O(N^2), Space Complexity = O(N) 
+void insertAndSort(stack<int> &s, int temp){
+    // basecase
+    if(s.empty() || s.top() < temp){
+        s.push(temp);
+    }
+    else{
+        int lastElement = s.top();
+        s.pop();
+        insertAndSort(s, temp);
+        s.push(lastElement);
+    }
+}
+
+void SortedStack :: sort(){
+   // basecase
+   if(s.empty()) return;
+   
+   int temp = s.top();
+   s.pop();
+   
+   sort();
+   insertAndSort(s, temp);
 }
