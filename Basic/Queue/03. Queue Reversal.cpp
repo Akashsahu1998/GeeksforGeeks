e593e7c8eef91b891c05cbe69e1f1ca02fea5987
Akash Sahu <akkusahu559@gmail.com>
2022-01-02 15:48:18 +0530

    Using Recursion

---
+++ Basic/Queue/03. Queue Reversal.cpp
@@ -0,0 +1,21 @@
+
+// Que) 03. Queue Reversal
+// Que Link :- https://practice.geeksforgeeks.org/problems/queue-reversal/1
+
+// Implementation
+
+// Time Complexity : O(N), Space Complexity : O(N)
+// Using recursion
+void reverse(queue<int> &q){
+    if(q.empty()) return;
+    int val = q.front();
+    q.pop();
+    reverse(q);
+    q.push(val);
+}
+
+queue<int> rev(queue<int> q){
+    reverse(q);
+    return q;
+}
+
