d75d108d393beb0064c579ef29f1f83b1b7d3739
Akash Sahu <akkusahu559@gmail.com>
2022-01-03 08:09:47 +0530

    Using DP

---
+++ Easy/DP/01. Rod Cutting.cpp
@@ -0,0 +1,32 @@
+
+// Que 01). Rod Cutting
+// Que Link :- https://practice.geeksforgeeks.org/problems/rod-cutting0840/1
+
+// Implementation
+
+// https://www.youtube.com/watch?v=eQuJb5gBkkc
+// Time Complexity = O(N), Space Complexity = O(N)
+
+class Solution{
+  public:
+    int cutRod(int price[], int n) {
+        vector<int> dp(n);
+        
+        dp[0] = price[0];
+        for(int itr = 1; itr < n; itr++){
+            dp[itr] = price[itr];
+            
+            // assuming 0 based indexing so, left + right = n-1
+            int left = 0;
+            int right = itr-1;
+            
+            while(left <= right){
+                dp[itr] = max(dp[itr], dp[left] + dp[right]);
+                left++;
+                right--;
+            }
+        }
+        
+        return dp[n-1];
+    }
+};
