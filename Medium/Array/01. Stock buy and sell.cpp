1f0ea2eaa29706a5d0f708582099824b3bde2c3d
Akash Sahu <akkusahu559@gmail.com>
2022-01-10 15:39:24 +0530

    Efficient Approach

--- Medium/Array/01. Stock buy and sell.cpp
+++ Medium/Array/01. Stock buy and sell.cpp
@@ -4,6 +4,8 @@
 
 // Implementation
 
+// Naive Approach
+// Using Stack
 // Time Complexity = O(N), Space Complexity = O(N)
 class Solution{
 public:
@@ -42,6 +44,33 @@ public:
             int firstIndex = st.top();
             st.pop();
             res.push_back({firstIndex, lastIndex});
+        }
+        return res;
+    }
+};
+
+
+
+// Efficient Approach
+// Time Complexity = O(N), Space Complexity = O(1)
+class Solution{
+public:
+    //Function to find the days of buying and selling stock for max profit.
+    vector<vector<int> > stockBuySell(vector<int> arr, int n){
+        vector<vector<int> > res;
+        
+        int store = 0;
+        for(int itr = 1; itr < n; itr++){
+            
+            // if last element was big then current element && the buy and sell day should not be same
+            if(arr[itr] < arr[itr-1] && store != itr-1){
+                res.push_back({store, itr-1});
+                store = itr;
+            }
+            // if its a last element && the buy and sell day should not be same
+            else if(itr == n-1 && store != itr-1){
+                res.push_back({store, itr});
+            }
         }
         return res;
     }
