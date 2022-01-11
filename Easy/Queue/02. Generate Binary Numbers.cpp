0beeb3d9b5b35e03c30d5f2fb7ba6ef25a711399
Akash Sahu <akkusahu559@gmail.com>
2022-01-11 08:06:48 +0530

    Using Queue, Efficient Approach

--- Easy/Queue/02. Generate Binary Numbers.cpp
+++ Easy/Queue/02. Generate Binary Numbers.cpp
@@ -20,3 +20,29 @@ vector<string> generate(int n){
 	
 	return res;
 }
+
+
+// https://www.geeksforgeeks.org/interesting-method-generate-binary-numbers-1-n/
+// Efficient Approach
+// Using Queue
+// Time Complexity = O(N), Space Complexity = O(NLogN)
+vector<string> generate(int n){
+	vector<string> res;
+	queue<string> q;
+	q.push("1");
+	
+	while(n--){
+	    string str1 = q.front();
+	    q.pop();
+	    res.push_back(str1);
+	    
+	    // Store str1 before changing it
+	    string str2 = str1;
+	    
+	    // apppend "0" to str1 & "1" to str2, and simultaneously push into q
+	    q.push(str1.append("0"));
+	    q.push(str2.append("1"));
+	}
+	
+	return res;
+}
