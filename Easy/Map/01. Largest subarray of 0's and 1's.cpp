e362449f198cad683f4d097b79849757a57a934d
Akash Sahu <akkusahu559@gmail.com>
2022-01-13 21:48:42 +0530

    Using unoredered_map

--- Easy/Map/01. Largest subarray of 0's and 1's.cpp
+++ Easy/Map/01. Largest subarray of 0's and 1's.cpp
@@ -12,18 +12,23 @@ class Solution{
         int maxLength = 0, sum = 0;
         unordered_map<int, int> mp;
         
+        // replacing 0 by -1
         for(int itr = 0; itr < n; itr++){
             arr[itr] = (arr[itr] == 0) ? -1 : 1;
         }
         
         for(int itr = 0; itr < n; itr++){
+        	
+        	// calculating the sum on each index
             sum += arr[itr];
             
-            // To handle sum=0 at last index, i.e = 0 0 1 1
+            // To handle sum = 0 at last index, i.e = 0 0 1 1
             if(sum == 0){
                 maxLength = itr+1;
             }
             
+            // if sum is already in the map, then just calculate the maximum length of the subarray
+            // if sum is not present then just add it into the map
             if(mp.find(sum) != mp.end()){
                 maxLength = max(maxLength, itr - mp[sum]);
             }
