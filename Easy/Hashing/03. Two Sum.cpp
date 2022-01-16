8b8c9d951a5a515a5d5bbb9e7f54548be267738d
Akash Sahu <akkusahu559@gmail.com>
2022-01-16 16:47:13 +0530

    Using unoredered set

--- Easy/Hashing/03. Two Sum.cpp
+++ Easy/Hashing/03. Two Sum.cpp
@@ -20,3 +20,22 @@ class Solution
         return false;
     }
 };
+
+
+// Using unoredered_set
+// Time Complexity = O(N), Space Complexity = O(N)
+class Solution
+{
+    public:
+    bool keypair(vector<int> arr, int n, int x){
+        unordered_set<int> st;
+        
+        for(int itr = 0; itr < n; itr++){
+            if(st.find(x - arr[itr]) != st.end()) return true;
+            else st.insert(arr[itr]);
+        }
+        
+        return false;
+    }
+};
+
