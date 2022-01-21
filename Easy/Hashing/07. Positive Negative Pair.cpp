c6b07f30133d5b06be9afb98269cf3d35d931372
Akash Sahu <akkusahu559@gmail.com>
2022-01-21 21:13:18 +0530

    Using unordered set

--- Easy/Hashing/07. Positive Negative Pair.cpp
+++ Easy/Hashing/07. Positive Negative Pair.cpp
@@ -34,3 +34,35 @@ class Solution{
         return res;
     }
 };
+
+
+// Using unoredered_set
+// Time Complexity = O(N), Space Complexity = O(N)
+class Solution{
+  public:
+    //Function to return list containing all the pairs having both
+    //negative and positive values of a number in the array.
+    vector <int> findPairs(int arr[], int n) {
+        // taking an st to store the presence of a val
+        unordered_set<int> st;
+        vector<int> res;
+        
+        // iterating over the elements
+        for(int itr = 0; itr < n; itr++){
+            int val = arr[itr];
+            
+            // if negative or positive value is present into the st then push both values into res and erase both values as well
+            if(st.find(val) != st.end() || st.find(-val) != st.end()){
+                val = abs(val);
+                res.push_back(-val);
+                res.push_back(val);
+                st.erase(val);
+                st.erase(-val);
+            }   // if not present into st, then simply add the value
+            else{
+                st.insert(val);
+            }
+        }
+        return res;
+    }
+};
