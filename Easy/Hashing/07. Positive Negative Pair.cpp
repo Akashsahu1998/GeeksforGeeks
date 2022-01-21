3a08b20e5643be3f509625402cef10a40b5dd129
Akash Sahu <akkusahu559@gmail.com>
2022-01-21 21:56:52 +0530

    Using unordered map

--- Easy/Hashing/07. Positive Negative Pair.cpp
+++ Easy/Hashing/07. Positive Negative Pair.cpp
@@ -19,48 +19,18 @@ class Solution{
         for(int itr = 0; itr < n; itr++){
             int val = arr[itr];
             
+            // val is positive then in map we are checking negative value is present or not
+            // val is negative then in map we are checking positive value is present or not
             // if negative or positive value is present into the mp then push both values into res and erase both values as well
-            if(mp[val] || mp[-val]){
+            if(mp[-val]){
+                mp[-val]--;
                 val = abs(val);
+                // push abs value into res two times
                 res.push_back(-val);
                 res.push_back(val);
-                mp.erase(val);
-                mp.erase(-val);
             }   // if not present into map, then simply add the value
             else{
                 mp[val]++;
-            }
-        }
-        return res;
-    }
-};
-
-
-// Using unoredered_set
-// Time Complexity = O(N), Space Complexity = O(N)
-class Solution{
-  public:
-    //Function to return list containing all the pairs having both
-    //negative and positive values of a number in the array.
-    vector <int> findPairs(int arr[], int n) {
-        // taking an st to store the presence of a val
-        unordered_set<int> st;
-        vector<int> res;
-        
-        // iterating over the elements
-        for(int itr = 0; itr < n; itr++){
-            int val = arr[itr];
-            
-            // if negative or positive value is present into the st then push both values into res and erase both values as well
-            if(st.find(val) != st.end() || st.find(-val) != st.end()){
-                val = abs(val);
-                res.push_back(-val);
-                res.push_back(val);
-                st.erase(val);
-                st.erase(-val);
-            }   // if not present into st, then simply add the value
-            else{
-                st.insert(val);
             }
         }
         return res;
