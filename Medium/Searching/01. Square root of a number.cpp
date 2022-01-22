f1a618f14f1e793be3a9a54ba4fa597b895eb591
Akash Sahu <akkusahu559@gmail.com>
2022-01-22 12:52:47 +0530

    2nd Approach

--- Medium/Searching/01. Square root of a number.cpp
+++ Medium/Searching/01. Square root of a number.cpp
@@ -4,9 +4,30 @@
 
 // Implementation
 
+// 1st Approach
 // Using Sqrt function
 // Time Complexity = O(LogN)
 // Space Complexity = O(1)
 long long int floorSqrt(long long int x) {
     return sqrt(x);
+}
+
+
+// 2nd Approach
+// Time Complexity = O(sqrt(n))
+// Space Complexity = O(1)
+long long int floorSqrt(long long int x) {
+    long long int res = -1;
+    for(long long int itr = 1; itr <= x; itr++){
+        long long int val = itr*itr;
+        if(val == x){
+            res = itr;
+            break;
+        }
+        else if(val > x){
+            res = itr-1;
+            break;
+        }
+    }
+    return res;
 }
