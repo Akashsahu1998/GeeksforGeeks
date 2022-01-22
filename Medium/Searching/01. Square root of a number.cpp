87cf8aebf1c5bf5b8c755017237b92f234476a3c
Akash Sahu <akkusahu559@gmail.com>
2022-01-22 12:51:20 +0530

    Using Sqrt function

---
+++ Medium/Searching/01. Square root of a number.cpp
@@ -0,0 +1,12 @@
+
+// Que 01). Square root of a number
+// Que Link :- https://practice.geeksforgeeks.org/problems/square-root/1
+
+// Implementation
+
+// Using Sqrt function
+// Time Complexity = O(LogN)
+// Space Complexity = O(1)
+long long int floorSqrt(long long int x) {
+    return sqrt(x);
+}
