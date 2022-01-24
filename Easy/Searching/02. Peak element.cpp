1f68cb511525b295e736104af9207e8e4b953204
Akash Sahu <akkusahu559@gmail.com>
2022-01-24 06:40:26 +0530

    Using binary search

--- Easy/Searching/02. Peak element.cpp
+++ Easy/Searching/02. Peak element.cpp
@@ -27,3 +27,31 @@ class Solution
        return 0;
     }
 };
+
+
+// 2nd Approach
+// Efficient Approach
+// Using binary search
+// Time Complexity = O(LogN)
+// Space Complexity = O(1)
+int peakElement(int arr[], int n){
+   int low = 0, high = n-1;
+   
+   while(low <= high){
+    //   int mid = low + (high - low)/2;
+        int mid = (low+high) >> 1;
+       
+       if((mid == 0 || arr[mid-1] < arr[mid]) && (mid == n-1 || arr[mid] > arr[mid+1])){
+           return mid;
+       }
+       
+       // if left element is big, so move towards left
+       if(mid > 0 && arr[mid-1] > arr[mid]){
+           high = mid-1;
+       }
+       else{    // else right is big, so move towards right
+           low = mid+1;
+       }
+   }
+   return 0;
+}
