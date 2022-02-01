a17b8510c92fbe9288989b9f1441ca24056a2c2c
Akash Sahu <akkusahu559@gmail.com>
2022-02-01 06:57:46 +0530

    Most Efficient Approach, Using 3 pointers

--- Easy/Array/04. Sort an array of 0s, 1s and 2s.cpp
+++ Easy/Array/04. Sort an array of 0s, 1s and 2s.cpp
@@ -44,3 +44,37 @@ class Solution
         }
     }
 };
+
+
+
+// 3rd Approach
+// Most Efficient Approach
+// Traversing 1 time
+// Using 3 pointers
+// Time Complexity = O(N)
+// Space Complexity = O(1)
+class Solution
+{
+    public:
+    void sort012(int nums[], int n){
+        int low = 0, mid = 0, high = n-1;
+        
+        // traversing over the array with the help of mid, run until and unless mid become greater than high
+        while(mid <= high){
+            
+            // if its 0, then swap low and mid values and increment low & mid by 1
+            if(nums[mid] == 0){
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){    // if its 1, then increment mid by 1
+                mid++;
+            }
+            else{    // if its 2, then swap mid and high values and decrement high by 1
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
