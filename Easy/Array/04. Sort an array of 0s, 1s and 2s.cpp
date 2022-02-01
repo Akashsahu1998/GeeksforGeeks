463b00e47cdb3be226ec3658e8ed7d30648ad9dd
Akash Sahu <akkusahu559@gmail.com>
2022-02-01 06:56:27 +0530

    Efficient Approach, Traversing 2 times over the array

--- Easy/Array/04. Sort an array of 0s, 1s and 2s.cpp
+++ Easy/Array/04. Sort an array of 0s, 1s and 2s.cpp
@@ -18,3 +18,29 @@ class Solution{
     }
     
 };
+
+
+// 2nd Approach
+// Efficient Approach
+// Traversing 2 times
+// Time Complexity = O(N)
+// Space Complexity = O(1)
+class Solution
+{
+    public:
+    void sort012(int nums[], int n){
+        int countZero = 0, countOne = 0, countTwo = 0;
+        
+        for(int itr = 0; itr < n; itr++){
+            if(nums[itr] == 0) countZero++;
+            else if(nums[itr] == 1) countOne++;
+            else countTwo++;
+        }
+        
+        for(int itr = 0; itr < n; itr++){
+            if(itr < countZero) nums[itr] = 0;
+            else if(itr < countZero + countOne) nums[itr] = 1;
+            else nums[itr] = 2;
+        }
+    }
+};
