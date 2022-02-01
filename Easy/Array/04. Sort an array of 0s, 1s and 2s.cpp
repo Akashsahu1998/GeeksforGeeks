96d8e849ca377031c87a9f3cb1662faeaa2718ce
Akash Sahu <akkusahu559@gmail.com>
2022-02-01 06:54:48 +0530

    Naive Approach, Using the inbuilt sort function

---
+++ Easy/Array/04. Sort an array of 0s, 1s and 2s.cpp
@@ -0,0 +1,20 @@
+
+// Que 04). Sort an array of 0s, 1s and 2s 
+// Que Link :- https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+
+
+// Implementation
+
+// 1st Approach
+// Naive Approach
+// Using the inbuilt sort function
+// Time Complexity = O(NLogN)
+// Space Complexity = O(1)
+class Solution{
+{
+    public:
+    void sort012(int a[], int n){
+        sort(a, a+n);
+    }
+    
+};
