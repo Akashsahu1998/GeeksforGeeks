4a6586c9a0aded58e3988cfeaaf31d7d825092fa
Akash Sahu <akkusahu559@gmail.com>
2022-05-02 21:43:33 +0530

    Using Merge Sort

--- Medium/LL/05. Flattening a Linked List.cpp
+++ Medium/LL/05. Flattening a Linked List.cpp
@@ -4,38 +4,41 @@
 
 // Implementation
 
-// Time Complexity = O(N), Space Complexity = O(1)
-Node *flatten(Node *root){
-   vector<int> vec;
-   Node *cur = root;
-   while(cur){
-       vec.push_back(cur->data);
-       if(cur->bottom){
-           // storing the cur->next
-           Node *bottomNode = cur->bottom, *storedNode = cur;
-           
-           // iterating over the bottom of the cur, till the end, and
-           // adding into set
-           while(bottomNode->bottom){
-               vec.push_back(bottomNode->data);
-               bottomNode = bottomNode->bottom;
-           }
-           vec.push_back(bottomNode->data);
-           cur = storedNode;
-       }
-       cur = cur->next;
-   }
-   
-   // sorting the vector to maintain the non decreasing order
-   sort(vec.begin(), vec.end());
-   
-   // creating a new dummy list
-   Node *flatteningNode = new Node(0);
-   Node *res = flatteningNode;
-   for(auto a : vec){ 
-       Node *newNode = new Node(a);
-       flatteningNode->bottom = newNode;
-       flatteningNode = flatteningNode->bottom;
-   }
-   return res->bottom;
+
+// Using Merge Sort
+// 1st Approach
+// Time Complexity = O(N)
+// Space Complexity = O(1)
+
+// merge two list
+Node* merge(Node* l1, Node* l2){
+    Node* res = new Node(0);
+    Node* temp = res;
+    
+    while(l1 && l2){
+        if(l1->data < l2->data){
+            temp->bottom = new Node(l1->data);
+            l1 = l1->bottom;
+        }
+        else{
+            temp->bottom = new Node(l2->data);
+            l2 = l2->bottom;
+        }
+        
+        temp = temp->bottom;
+    }
+    
+    if(l1) temp->bottom = l1;
+    if(l2) temp->bottom = l2;
+    
+    return res->bottom;
+}
+
+Node* flatten(Node *root){
+  if(!root || !root->next) return root;
+  
+  root->next = flatten(root->next);
+  
+  root = merge(root, root->next);
+  return root;
 }
