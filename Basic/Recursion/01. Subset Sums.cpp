b35957df70670ae87926a429071a657bfacb3775
Akash Sahu <akkusahu559@gmail.com>
2022-05-13 22:42:36 +0530

    3 Approaches

--- Basic/Recursion/01. Subset Sums.cpp
+++ Basic/Recursion/01. Subset Sums.cpp
@@ -4,6 +4,7 @@
 
 // Implementation
 
+// 1st Approach
 // Using Recursion
 // Time Complexity : O(2^n)
 // Space Complexity : O(2^n)
@@ -27,6 +28,54 @@ public:
     vector<int> subsetSums(vector<int> arr, int N){
         vector<int> res;
         calculateSubsets(res, arr, 0, 0);
+        return res;
+    }
+};
+
+
+
+// 2nd Approach
+// Using Recursion
+// Time Complexity : O(2^n)
+// Space Complexity : O(2^n)
+class Solution{
+private:
+    void calculateSubsets(vector<int> &res, vector<int> arr, int index, int sum){
+        res.push_back(sum);
+        
+        for(int i = index; i < arr.size(); i++){
+            calculateSubsets(res, arr, i+1, sum+arr[i]);
+        }
+    }
+
+public:
+    vector<int> subsetSums(vector<int> arr, int N){
+        vector<int> res;
+        calculateSubsets(res, arr, 0, 0);
+        return res;
+    }
+};
+
+
+
+// 3rd Approach
+// Iterative Approach
+// Time Complexity: O(N * 2^N)
+// Space Complexity: O(2^N)
+class Solution{
+public:
+    vector<int> subsetSums(vector<int> arr, int N){
+        vector<int> res;
+        res.push_back({});
+        
+        for(int i = 0; i < arr.size(); i++){            
+            int resSize = res.size();            
+            for(int j = 0; j < resSize; j++){
+                int sum = res[j];
+                sum += arr[i];
+                res.push_back(sum);
+            }
+        }        
         return res;
     }
 };
