30b1f7da0f02b5a140852137a1d9d9589c1d87c6
Akash Sahu <akkusahu559@gmail.com>
2022-05-31 15:03:17 +0530

    Using BFS, Kahn's Algo

--- Medium/Graph/02. Detect cycle in a directed graph.cpp
+++ Medium/Graph/02. Detect cycle in a directed graph.cpp
@@ -44,3 +44,49 @@ class Solution{
         return false;
     }
 };
+
+
+// 1st Approach
+// Using BFS, Kahn's Algo
+// Time Complexity = O(N+E)
+// Space Complexity = (O(N) + O(N)) => O(N)
+// Where N is the nunber of vertices in the graph and E is the number of edges
+class Solution{
+public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int v, vector<int> adj[]) {
+        vector<int> inDegree(v, 0);
+        
+        for(int i = 0; i < v; i++){
+            for(auto a : adj[i]){
+                inDegree[a]++;
+            }
+        }
+        
+        queue<int> q;
+        for(int i = 0; i < v; i++){
+            if(inDegree[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        // if there is cycle so, it will not count all nodes
+        // if there is no cycle so, it will count all nodes present into graph
+        int cnt = 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            cnt++;
+            
+            for(auto a : adj[node]){
+                inDegree[a]--;
+                
+                if(inDegree[a] == 0){
+                    q.push(a);
+                }
+            }
+        }
+        
+        return !(cnt == v);
+    }
+};
